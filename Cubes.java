be11907f7fd5f79e09953a28bece4ff1e39508fc
rubylicious88 <rubywright26@marlborough.org>
2025-09-23 14:11:30 -0700

    Add files via upload
    Unit 1 assignments

---
+++ Cubes.java
@@ -0,0 +1,10 @@
+public class Cubes {
+    public static void main(String[] args) {
+        int numTerms = 10; 
+
+        for (int i = 0; i < numTerms; i++) {
+            int cube = i * i * i;
+            System.out.println(cube);
+        }
+    }
+}
