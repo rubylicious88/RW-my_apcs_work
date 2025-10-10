142c85fe5bc87c37ff5f66e055c3334a9cc77727
rubylicious88 <rubywright26@marlborough.org>
2025-10-10 09:13:05 -0700

    Everything

---
+++ unit1 assignments /Fibonacci.java output.java
@@ -0,0 +1,39 @@
+Last login: Tue Sep 23 14:06:36 on ttys002
+/var/folders/g8/7b2sklrx4gd768xs19nxs7dh0000gn/T/geany_run_script_9QHED3.sh ; exit;
+rubywright@MacBookAir ~ % /var/folders/g8/7b2sklrx4gd768xs19nxs7dh0000gn/T/geany_run_script_9QHED3.sh ; exit;
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+2584
+4181
+6765
+10946
+17711
+28657
+46368
+75025
+121393
+196418
+317811
+514229
+832040
+
+
+------------------
+(program exited with code: 0)
+Press return to continue
+
---
+++ unit1 assignments /MutipleO3.java output.java
@@ -0,0 +1,22 @@
+Last login: Tue Sep 23 13:32:54 on ttys001
+/var/folders/g8/7b2sklrx4gd768xs19nxs7dh0000gn/T/geany_run_script_89DND3.sh ; exit;
+rubywright@MacBookAir ~ % /var/folders/g8/7b2sklrx4gd768xs19nxs7dh0000gn/T/geany_run_script_89DND3.sh ; exit;
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+
+
+------------------
+(program exited with code: 0)
+Press return to continue
+
+
+
+
